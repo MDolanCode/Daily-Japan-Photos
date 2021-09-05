420301cf7895b9994552d4d1284382069fca149e
Matt Dolan <mdolan.code@gmail.com>
2021-09-05 13:21:03 -0700

    Import Alamofire, setup url, and array to hold the data. Make networking request.

--- Daily Japan Photos/DailyJapanPhotosVC.swift
+++ Daily Japan Photos/DailyJapanPhotosVC.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Alamofire
 
 class DailyJapanPhotosVC: UIViewController {
-
+    
+    let url = "https://pixabay.com/api/?key=23250900-37b6c2e019e67545c2686f55c&q=japan&image_type=photo"
+    
+    let japanPhoto: [JapanPhoto] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        AF.request(self.url, method: .get).responseDecodable(of: [JapanPhoto].self) { (response) in
+            debugPrint(response)
+        }
     }
-
-
 }
+
 
