2907f2f44afcb3a3f4d66295a9e90c1b7843c295
Matt Dolan <mdolan.code@gmail.com>
2021-09-05 14:23:30 -0700

    Register nib/cell and add tableView datasource in DailyJapanPhotosVC.

--- Daily Japan Photos/Controller/DailyJapanPhotosVC.swift
+++ Daily Japan Photos/Controller/DailyJapanPhotosVC.swift
@@ -14,15 +14,33 @@ class DailyJapanPhotosVC: UIViewController {
     
     let url = "https://pixabay.com/api/?key=23250900-37b6c2e019e67545c2686f55c&q=japan&image_type=photo"
     
-    let japanPhoto: [JapanPhoto] = []
+    let japanPhoto: [JapanPhotoModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        AF.request(self.url, method: .get).responseDecodable(of: [JapanPhoto].self) { (response) in
+        tableView.dataSource = self
+        
+        tableView.register(DailyJapanPhotosTVC.nib(), forHeaderFooterViewReuseIdentifier: DailyJapanPhotosTVC.identifier)
+        
+        AF.request(self.url, method: .get).responseDecodable(of: [JapanPhotoModel].self) { (response) in
             debugPrint(response)
         }
     }
+}
+
+extension DailyJapanPhotosVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DailyJapanPhotosTVC.identifier, for: indexPath) as! DailyJapanPhotosTVC
+        
+        return cell
+    }
+    
+    
 }
 
 
