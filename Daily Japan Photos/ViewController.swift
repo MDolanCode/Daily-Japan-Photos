62382b4a3aa7bb558c0e0f697ab6222d56239052
Matt Dolan <mdolan.code@gmail.com>
2021-09-05 12:06:34 -0700

    Initial Commit

---
+++ Daily Japan Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily Japan Photos
+//
+//  Created by Matt Dolan External macOS on 2021-09-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
