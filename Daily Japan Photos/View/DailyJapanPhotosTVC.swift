18afefed9f3ebf7e358cd24b43009d58ba381d2c
Matt Dolan <mdolan.code@gmail.com>
2021-09-05 13:30:46 -0700

    Create DailyJapanPhotos TableViewCell and xib.

---
+++ Daily Japan Photos/View/DailyJapanPhotosTVC.swift
@@ -0,0 +1,20 @@
+//
+//  DailyJapanPhotosTVC.swift
+//  Daily Japan Photos
+//
+//  Created by Matt Dolan External macOS on 2021-09-05.
+//
+
+import UIKit
+
+class DailyJapanPhotosTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
